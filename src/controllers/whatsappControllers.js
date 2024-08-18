731caa691ac7e8438923adb3819dd166b58cb22f
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-08-18 01:06:55 -0500

    Desarrollar verifyToken

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,19 @@
 const VerifyToken = (req, res) => {
-    res.send("Hola verifyToken");
+    
+    try {
+        const accessToken = process.env.TOKEN_WHATSAPP;
+        const token = req.query["hub.verify_token"];
+        const challenge = req.query["hub.challenge"]
+
+        if(challenge != null && token != null && token != accessToken){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
+
+    }catch(error){
+        res.status(400).send();
+    }
 }
 
 const ReceivedMessage = (req, res) => {
